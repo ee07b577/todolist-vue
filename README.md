2ef78b0b66eb84ff44ce583574df40af3687d38b
梁芳芳 <liangfangfang@liangfangfangdeMacBook-Pro.local>
2017-07-12 18:13:23 +0800

    gulp build task to build the project

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
     2. $npm install
     3. $gulp serve
     4. develop and you can see the live page when saving files
-
+    5. $gulo build // build the project
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var connect = require('gulp-connect')
     // var livereload = require('gulp-livereload')
 var browserSync = require('browser-sync').create()
 
+// for build
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+var clean = require('gulp-clean')
+var htmlmin = require('gulp-htmlmin')
+var htmlrepalce = require('gulp-html-replace')
 
 gulp.task('serve', function() {
     browserSync.init({
@@ -17,4 +23,26 @@ gulp.task('serve', function() {
     gulp.watch(['src/**/*.*'], function() {
         console.log('js changed')
     }).on('change', browserSync.reload)
+})
+
+gulp.task('build', ['clean'], function() {
+    gulp.src(['src/js/vue.js', 'src/js/test.js'])
+        .pipe(concat('bundle.min.js', { newLine: ";" }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+    gulp.src('src/*.html')
+        .pipe(htmlrepalce({
+            'js': 'js/bundle.min.js'
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            minifyJS: true,
+            minifyCSS: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('clean', function() {
+    return gulp.src('dist', { read: false }).pipe(clean())
 })
